7e74b79293e14b0a5d7d2a94597ea8e0d6ad238e
p0ptartz <ericwm420@gmail.com>
2023-03-03 23:19:43 -0500

    basic navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import Navbar from './components/Navbar';
 import './App.css';
+
+
 
 function App() {
   return (
     <div>
-
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <section className="nav-container">
+            <nav>
+                <a href="#">RESUME </a>
+                <a href="#">SKILLS</a>
+                <a href="#">PROJECTS</a>
+                <a href="#">CONTACT</a>
+            </nav>
+
+        </section>
+
+    )
+}
+
+export default Navbar
