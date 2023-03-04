23dc86eba8f1aa84c76f64e3e02ee009134c9dc5
p0ptartz <ericwm420@gmail.com>
2023-03-04 01:19:16 -0500

    added img to landing body

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,14 +2,27 @@ import React from "react";
 
 function Landing() {
     return (
-        <section className="landing-body">
-            <div className="welcome">
-                <h1>Welcome,</h1>
+
+        <div>
+
+            <div className="landing-container">
+                <section className="landing-body">
+                    <div className="welcome">
+                        <h1>Welcome,</h1>
+                    </div>
+                    <div className="about">
+                        <p>my name is p0ptartz and i'm a fullstack web developer based in New Jersey.  I've deployed scalable websites using HTML, CSS, JS and React. <br /> <br /> When i'm not working on a project, you can find me chugging beers in my room and punching holes in my wall.</p>
+                    </div>
+                </section>
+                <div className="img-body">
+                    <div className="img">
+
+                    </div>
+
+                </div>
             </div>
-            <div className="about">
-                <p>my name is p0ptartz and i'm a fullstack web developer based in New Jersey.  I've deployed scalable websites using HTML, CSS, JS and React. <br /> <br /> When i'm not working on a project, you can find me chugging beers in my room and punching holes in my wall.</p>
-            </div>
-        </section>
+
+        </div>
 
     )
 }
