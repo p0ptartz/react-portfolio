4995055373c08387c528daa79e3596608a6529c3
p0ptartz <ericwm420@gmail.com>
2023-03-04 00:22:22 -0500

    started main body

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar';
+import Landing from './components/Landing';
 import './App.css';
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <Navbar />
+      <Landing />
     </div>
   );
 }
---
+++ src/components/Landing.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Landing() {
+    return (
+        <section className="landing-body">
+            <div className="welcome">
+                <h1>Welcome,</h1>
+            </div>
+            <div className="about">
+                <p>my name is p0ptartz and i'm a fullstack web developer based in New Jersey.  I've deployed scalable websites using HTML, CSS, JS and React. <br /> <br /> When i'm not working on a project, you can find me chugging beers in my room and punching holes in my wall.</p>
+            </div>
+        </section>
+
+    )
+}
+
+export default Landing
