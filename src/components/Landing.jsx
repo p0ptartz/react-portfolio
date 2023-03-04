988a21d57202c6ee8ffd62203aa64be79bf7d787
p0ptartz <ericwm420@gmail.com>
2023-03-04 02:56:14 -0500

    changed divider

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -11,7 +11,7 @@ function Landing() {
                         <h1>Welcome,</h1>
                     </div>
                     <div className="about">
-                        <p>my name is p0ptartz and i'm a fullstack web developer based in New Jersey.  I've deployed scalable websites using HTML, CSS, JS and React. <br /> <br /> When i'm not working on a project, you can find me chugging beers in my room and punching holes in my wall.</p>
+                        <p>my name is p0ptartz and i'm a fullstack web developer based in New Jersey.  I've deployed scalable websites using HTML, CSS, JS and React. <br /> <br /> When i'm not working on a project, you can find me chugging beers in my basement while punching holes in the wall.</p>
                     </div>
                 </section>
                 <div className="img-body">
@@ -23,6 +23,8 @@ function Landing() {
             </div>
             <div className="line-container">
                 <div className="line"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
             </div>
 
         </div>
