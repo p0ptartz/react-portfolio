3f8259cc4fd548fe98e0a0b790a94c7ec16404c1
p0ptartz <ericwm420@gmail.com>
2023-03-04 01:45:08 -0500

    added to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,9 @@ import React from "react";
 function Navbar() {
     return (
         <section className="nav-container">
+            <div className="logo">
+                <h1>eric<span className="last-name">Meyers</span></h1>
+            </div>
             <nav>
                 <a href="#">RESUME </a>
                 <a href="#">SKILLS</a>
