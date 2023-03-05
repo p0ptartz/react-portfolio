101f1b4b652ae997748010f2f5ee72432eebb5f2
p0ptartz <ericwm420@gmail.com>
2023-03-05 00:56:03 -0500

    started portfolio section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 import Skills from './components/Skills';
+import Portfolio from './components/Portfolio';
 import './App.css';
 
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Landing />
       <Skills />
+      <Portfolio />
     </div>
   );
 }
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Portfolio() {
+    return (
+        <div>
+            <div className="port-title">
+                <h1>Portfolio:</h1>
+            </div>
+            <div className="proj-1">
+                <div className="proj-1-img"></div>
+                <div className="proj-1-text">
+                    <div className="proj-1-title">
+                        <h2>PROJECT 1</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quos libero blanditiis! Non ea et consequatur nobis rerum corporis? Maiores, assumenda atque? Modi cumque consequatur impedit explicabo. Laborum, dolores recusandae!</p>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Portfolio
