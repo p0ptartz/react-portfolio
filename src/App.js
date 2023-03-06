cc52c76b5c654fc89cc59ec5f95d94e6b7031330
p0ptartz <ericwm420@gmail.com>
2023-03-05 23:45:45 -0500

    updated portfolio

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 import Skills from './components/Skills';
 import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 import './App.css';
 
 
@@ -13,6 +14,7 @@ function App() {
       <Landing />
       <Skills />
       <Portfolio />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function Contact() {
+    return (
+        <div>
+            <section className="contact-me">
+                <h1>Contact Me:</h1>
+            </section>
+            <form className="contact-form" action="#" method="post">
+                <div className="contact-left">
+
+                    <input type="text" id="name" name="name" required placeholder="Name*"></input>
+
+                    <input type="email" id="email" name="email" required placeholder="E-mail*"></input>
+
+                    <input type="text" id="subject" name="subject" required placeholder="Subject*"></input>
+
+                    <textarea id="message" name="message" required placeholder="Please write your message here"></textarea>
+
+                    <input type="submit" value="Send" id="submit"></input>
+                </div>
+                <div className="contact-right">
+
+                    <a href="#" className="contact-img-1"></a>
+                    <a href="#" className="contact-img-2"></a>
+                    <a href="#" className="contact-img-3"></a>
+
+                </div>
+            </form >
+        </div>
+
+
+
+
+    )
+}
+
+
+export default Contact
